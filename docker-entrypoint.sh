62142b39ea5ac6251487b2addeb2d1714eb86cf7
Nick Vissari <networknerd@gmail.com>
2022-07-11 15:33:12 -0400

    Fix create config from env vars

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,14 +7,14 @@ CONFIG="/opt/duoauthproxy/conf/authproxy.cfg"
 
 # create config file from env vars
 env | grep -q DUO_CLOUD && echo '[cloud]' > $CONFIG
-test ! -z "$DUO_CLOUD_IKEY" && echo "$DUO_CLOUD_IKEY" >> $CONFIG
-test ! -z "$DUO_CLOUD_SKEY" && echo "$DUO_CLOUD_SKEY" >> $CONFIG
-test ! -z "$DUO_CLOUD_SKEY_FILE" && echo $(cat "$DUO_CLOUD_SKEY_FILE") >> $CONFIG
-test ! -z "$DUO_CLOUD_API_HOST" && echo "$DUO_CLOUD_API_HOST" >> $CONFIG
+test ! -z "$DUO_CLOUD_IKEY" && echo "ikey=$DUO_CLOUD_IKEY" >> $CONFIG
+test ! -z "$DUO_CLOUD_SKEY" && echo "skey=$DUO_CLOUD_SKEY" >> $CONFIG
+test ! -z "$DUO_CLOUD_SKEY_FILE" && echo "skey=$(cat "$DUO_CLOUD_SKEY_FILE")" >> $CONFIG
+test ! -z "$DUO_CLOUD_API_HOST" && echo "api_host=$DUO_CLOUD_API_HOST" >> $CONFIG
 env | grep -q DUO_CLOUD && echo >> $CONFIG # add a new line
-test ! -z "$SERVICE_ACCOUNT_USERNAME" && echo "$SERVICE_ACCOUNT_USERNAME" >> $CONFIG
-test ! -z "$SERVICE_ACCOUNT_PASSWORD" && echo "$SERVICE_ACCOUNT_PASSWORD" >> $CONFIG
-test ! -z "$SERVICE_ACCOUNT_PASSWORD_FILE" && echo $(cat "$SERVICE_ACCOUNT_PASSWORD_FILE") >> $CONFIG
+test ! -z "$SERVICE_ACCOUNT_USERNAME" && echo "service_account_username=$SERVICE_ACCOUNT_USERNAME" >> $CONFIG
+test ! -z "$SERVICE_ACCOUNT_PASSWORD" && echo "service_account_password=$SERVICE_ACCOUNT_PASSWORD" >> $CONFIG
+test ! -z "$SERVICE_ACCOUNT_PASSWORD_FILE" && echo "service_account_password=$(cat "$SERVICE_ACCOUNT_PASSWORD_FILE")" >> $CONFIG
 
 # start duo stuff
 /etc/init.d/duoauthproxy start
